af099ae2d615373950edd2aa9a75f19bc6395a10
Theo Theunissen <theo.theunissen@gmail.com>
2020-02-14 09:16:44 +0100

    voorbeeldje toegeveoegd

--- myrun.sh
+++ myrun.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#
+# Run this with wget -qO- https://raw.githubusercontent.com/boschpeter/theotheu/master/myrun.sh | bash
+#
+
 git clone https://github.com/boschpeter/theotheu
 
 echo "localhost" >> hosts
